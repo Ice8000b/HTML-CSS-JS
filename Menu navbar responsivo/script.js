a0087838616126712f61e10f89afb1d887d26487
Ice8000b <adrielluiz73@gmail.com>
2022-01-22 15:55:48 -0200

    first commit

--- Menu navbar responsivo/script.js
+++ Menu navbar responsivo/script.js
@@ -1,17 +1,21 @@
-const mobile = document.querySelector('.mobile-menu')
-const menu = document.querySelector('.nav-list')
-mobile.addEventListener('click', active)
-let verify = 1
+const mobile_menu = document.querySelector('.mobile-menu')
+const nav_list = document.querySelector('.nav-list')
+const nav_links = document.querySelectorAll('li')
+mobile_menu.addEventListener('click', active)
+var verify = 1
 
 function active() {
     if (verify == 1) {
-        menu.style.width = '50vw'
-        menu.style.opacity = '1'
-        menu.style.transition = '0.4s'
+        nav_list.style.width = '50vw'
+        nav_links.forEach((links, index) => {
+            links.style.animation ? (links.style.animation = '') : (links.style.animation = `navLinkFade 0.3s forwards ${index / 7 + 0.3}s`)
+        })
         verify = 0
     } else {
-        menu.style.width = '0'
-        menu.style.opacity = '0'
+        nav_list.style.width = '0'
+        nav_links.forEach((links, index) => {
+            links.style.animation ? (links.style.animation = '') : (links.style.animation = `navLinkFade 0.3s forwards${index /7 + 0.3}s`)
+        })
         verify = 1
     }
 }
