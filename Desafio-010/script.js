0eef7b1afcd6537bfe4079eea9f6bc3ec25e093a
Ice8000b <adrielluiz73@gmail.com>
2022-02-17 12:10:20 -0200

    first commit

--- Desafio-010/script.js
+++ Desafio-010/script.js
@@ -1,22 +1,11 @@
 const mobile_menu = document.querySelector('.mobile-menu')
 const nav_list = document.querySelector('.nav-list')
-const navlinks = document.querySelectorAll('li')
+const nav_links = document.querySelectorAll('.nav-list>li')
 mobile_menu.addEventListener('click', active)
-let verify = 1
 
 function active() {
-    if (verify == 1) {
-        nav_list.style.width = '50vw'
-        nav_list.style.transition = '0.3s'
-        navlinks.forEach((link, index) => {
-            link.style.animation ? (link.style.animation = '') : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
-        })
-        verify = 0
-    } else {
-        nav_list.style.width = '0'
-        navlinks.forEach((link) => {
-            link.style.animation ? (link.style.animation = '') : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s')
-        })
-        verify = 1
-    }
+    nav_list.classList.toggle('active')
+    nav_links.forEach((link, index) => {
+        link.style.animation ? (link.style.animation = '') : (link.style.animation = `navLinkFade ease 0.3s forwards ${index / 8 + 0.2}s`)
+    })
 }
